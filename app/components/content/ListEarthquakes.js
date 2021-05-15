0fd9bae003511ec4450b62a3b07aa6194f03ff86
bogutskii <sl9site@gmail.com>
2021-05-14 21:58:32 -0700

    -added sorting of the list

--- app/components/content/ListEarthquakes.js
+++ app/components/content/ListEarthquakes.js
@@ -1,23 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {NavLink} from "react-router-dom";
 
 function ListEarthquakes(props) {
+    const [typeSorting, setTypeSorting] = useState({direction: true, column: 'place'})
+
+    function placeSplit(obj) {
+        if (typeSorting.column === 'place') {
+            return obj.place.split('km')[0]
+        }
+        return obj[typeSorting.column]
+    }
+
+    const sortList = (title) => {
+        if (typeSorting.column === title) {
+            setTypeSorting({...typeSorting, direction: !typeSorting.direction})
+        } else {
+            setTypeSorting({column: title, direction: true})
+        }
+    }
     return (
+
         <div>
+            <button onClick={() => sortList()}>bbb</button>
             <h1 className="title-h">{props.title}</h1>
             <table>
                 <tbody>
                 <tr>
-                    <td className="center-text">Title</td>
-                    <td className="center-text">Magnitude</td>
-                    <td className="center-text">Time</td>
+                    <td className="center-text" onClick={() => sortList('place')}>Title</td>
+                    <td className="center-text" onClick={() => sortList('mag')}> Magnitude</td>
+                    <td className="center-text" onClick={() => sortList('time')}> Time</td>
                 </tr>
-                {[...props.features].sort((a, b) => b.properties.mag - a.properties.mag).map(el => <tr key={el.id}>
-                    <td className="title-h bold"><NavLink to={`/detail/${el.id}`}
-                                                          className="link"> {el.properties.place} </NavLink></td>
-                    <td className="center-text title-h bold">{el.properties.mag}   </td>
-                    <td className="title-h bold">{getData(el.properties.time)}   </td>
-                </tr>)}
+                {
+                    [...props.features].sort((a, b) => typeSorting.direction ?
+                        placeSplit(b.properties) - placeSplit(a.properties)
+                        :
+                        placeSplit(a.properties) - placeSplit(b.properties)
+                    ).map(el =>
+                        <tr key={el.id}>
+                            <td className="title-h bold"><NavLink to={`/detail/${el.id}`}
+                                                                  className="link"> {el.properties.place} </NavLink>
+                            </td>
+                            <td className="center-text title-h bold">{el.properties.mag} </td>
+                            <td className="title-h bold">{getData(el.properties.time)}</td>
+                        </tr>
+                    )
+                }
                 </tbody>
             </table>
         </div>
